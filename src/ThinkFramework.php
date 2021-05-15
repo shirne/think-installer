911bf34406066665fe8e671044af5b702a44bd92
shirne <shirne@126.com>
2021-05-15 17:35:29 +0800

    rename

--- src/ThinkFramework.php
+++ src/ThinkFramework.php
@@ -24,7 +24,7 @@ class ThinkFramework extends LibraryInstaller
      */
     public function getInstallPath(PackageInterface $package)
     {
-        if ('topthink/framework' !== $package->getPrettyName()) {
+        if ('shirne/thinkphp' !== $package->getPrettyName()) {
             throw new InvalidArgumentException('Unable to install this library!');
         }
 
