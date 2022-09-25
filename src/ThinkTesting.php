93030ef8e52992d92661df28110aa365ce60adf0
shirne <shirne@126.com>
2022-09-25 12:27:42 +0800

    Update ThinkTesting.php

--- src/ThinkTesting.php
+++ src/ThinkTesting.php
@@ -22,7 +22,7 @@ class ThinkTesting extends LibraryInstaller
      */
     public function getInstallPath(PackageInterface $package)
     {
-        if ('topthink/think-testing' !== $package->getPrettyName()) {
+        if ('shirne/think-testing' !== $package->getPrettyName()) {
             throw new InvalidArgumentException('Unable to install this library!');
         }
 
